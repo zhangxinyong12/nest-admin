d360fc77aeed5fe884bada16f0b155581c4767fe
张新永 <xinyong.zhang@zen-x.com.cn>
2023-04-12 17:28:04 +0800

    feat: 添加pipe管道校验

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { generateDocument } from './doc';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 添加全局的管道
+  app.useGlobalPipes(new ValidationPipe());
+
   // 场景文档
   await generateDocument(app);
 
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,11 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-
+import {
+  IsNotEmpty,
+  Matches,
+  Max,
+  Min,
+  Length,
+  IsEmail,
+} from 'class-validator';
 export class CreateUserDto {
   @ApiProperty({
     description: '手机号',
     type: String,
     example: '13800138000',
   })
+  @Matches(/^1\d{10}$/g, { message: '请输入手机号' })
   phone: string;
 
   @ApiProperty({
@@ -16,11 +24,14 @@ export class CreateUserDto {
   @ApiProperty({
     example: '123456',
   })
+  @IsNotEmpty({ message: '密码不能为空' })
+  @Length(6, 16, { message: '密码长度为6-16位' })
   password: string;
 
   @ApiProperty({
     example: '11111111@qq.com',
     required: false,
   })
+  @IsEmail({}, { message: '请输入正确的邮箱' })
   email: string;
 }
