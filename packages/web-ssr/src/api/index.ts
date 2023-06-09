64a9c3e8fc31b732b59d33cf0f5f64f2ec8f58c5
张新永 <xinyong.zhang@zen-x.com.cn>
2023-06-09 14:22:39 +0800

    feat: 封装http函数

--- packages/web-ssr/src/api/index.ts
+++ packages/web-ssr/src/api/index.ts
@@ -1,18 +1,16 @@
+import Http from "@/app/utils/http"
+
 export async function getTangshiList() {
-  const { data } = await fetch("http://localhost:3000/tangshi", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
+  const { data } = await Http.post(
+    "http://localhost:3000/tangshi",
+    {
       page: 1,
       pageSize: 2,
-    }),
-    next: {
-      // revalidate: 1000, // 请求结果缓存*s
-      tags: ["refresh"], // 缓存标签
     },
-    // cache: "no-cache", // 不缓存
-  }).then((res) => res.json())
+    {
+      revalidate: 1000, // 请求结果缓存*s
+      tags: ["refresh"], // 缓存标签
+    }
+  )
   return data.items
 }
--- packages/web-ssr/src/app/utils/http.ts
+++ packages/web-ssr/src/app/utils/http.ts
@@ -1,4 +1,35 @@
 // 封装 HTTP 请求函数
-class HTTP {}
+class HTTP {
+  get(url: string, data?: any, next?: any, cache?: RequestCache) {
+    let params = ""
+    if (data) {
+      Object.keys(data).forEach((key) => {
+        params += `${key}=${data[key]}&`
+      })
+      params = params.slice(0, -1)
+      url = `${url}?${params}`
+    }
+    return fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      next,
+      cache,
+    }).then((res) => res.json())
+  }
+  post(url: string, data?: any, next?: any, cache?: RequestCache) {
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data || {}),
+      next,
+      cache,
+    }).then((res) => res.json())
+  }
+}
 
-export default HTTP
+// eslint-disable-next-line import/no-anonymous-default-export
+export default new HTTP()
