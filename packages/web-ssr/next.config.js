00b6ede826f6882f080692b02d5a495e83e4bc56
张新永 <xinyong.zhang@zen-x.com.cn>
2023-06-12 10:14:48 +0800

    feat: 导出静态的html。但是不能使用服务端渲染

--- packages/web-ssr/next.config.js
+++ packages/web-ssr/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // output: "export", // 导出静态的html。但是不能使用服务端渲染
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
 
 module.exports = nextConfig
