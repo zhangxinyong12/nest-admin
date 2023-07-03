2b88719d2f82d51e678f33e6bf96bd2dd42cc35d
zhangxinyong <13271150671@wo.cn>
2023-07-04 00:07:53 +0800

    feat: 添加环境变量

--- packages/server/src/shared/configs/module-options.ts
+++ packages/server/src/shared/configs/module-options.ts
@@ -1,8 +1,8 @@
 import { ConfigModuleOptions } from '@nestjs/config/dist/interfaces';
 
 import configuration from './configuration';
-
+const NODE_ENV = process.env.NODE_ENV || 'prod';
 export const configModuleOptions: ConfigModuleOptions = {
-  envFilePath: '.env',
+  envFilePath: `.env.${NODE_ENV}`,
   load: [configuration],
 };
